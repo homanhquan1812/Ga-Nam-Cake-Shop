f4bcafd19bbf4747abe0f47243e052a34b201a3e
Hồ Mạnh Quân <homanhquan1812@gmail.com>
2023-12-23 09:37:23 +0700

    Update

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,7 +1,6 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
@@ -11,9 +10,6 @@ registerLink.addEventListener('click', ()=> {
 loginLink.addEventListener('click', ()=> {
   wrapper.classList.remove('active');
   document.querySelector('.button-active').style.display = 'none';
-});
-btnPopup.addEventListener('click', ()=> {
-  wrapper.classList.add('active-popup');
 });
 iconClose.addEventListener('click', ()=> {
   wrapper.classList.remove('active-popup');
