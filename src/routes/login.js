26da71d59707ee388f37055da911f65deec43b88
Hồ Mạnh Quân <homanhquan1812@gmail.com>
2024-04-26 19:28:07 +0700

    Update login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,4 +5,5 @@ const loginController = require('../app/controllers/LoginController');
 router.get('/', loginController.index)
 router.post('/loginSubmit', loginController.login)
 router.post('/registerSubmit', loginController.register)
+
 module.exports = router;
