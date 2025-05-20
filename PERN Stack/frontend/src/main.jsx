4d83d7ae73be2f7874de09f72829ffea48e57593
Hồ Mạnh Quân <homanhquan1812@gmail.com>
2025-05-20 09:28:05 +0700

    New Update

--- PERN Stack/frontend/src/main.jsx
+++ PERN Stack/frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { StoreProvider } from "./store/store.jsx";
+import { StoreProvider } from "./store/Store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
